e3fea72140824bc7ecb8f32d0f0d28eee7a9e221
Marcus Rana <marcusrana01@gmail.com>
2020-03-26 11:59:06 -0500

    Initialized project
    Added unity project to repository

---
+++ aero-simulator/Assets/BallisticMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Math = System.Math;
+
+public class BallisticMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Transform rocket;
+    public Transform target;
+    
+    //sets time is time until impact in seconds
+    public float time = 10f;
+    public bool hasLaunched = false;
+    //g is gravity coefficient, assigned to 9.81 by Unity
+    public float g = Physics.gravity.y;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("i") && hasLaunched == false){
+            //get distance between objects
+            float deltaX  = target.position.x - rocket.position.x;
+            float deltaY = target.position.y - rocket.position.y;   
+            float deltaZ = target.position.z - rocket.position.z;
+
+            //calculate the velocity in the Y direction
+            double Double = Math.Pow(time,2);
+            float timeSquared = Convert.ToSingle(Double);
+            double doubleVelocityY= (deltaY - (.5*g*(timeSquared)))/time;
+            float velocityY = Convert.ToSingle(doubleVelocityY);
+
+            //assigns velocities to x y z components
+            float initYVelocity = velocityY;
+            float initXVelocity = deltaX/time;
+            float initZVelocity = deltaZ/time;
+
+            //applies initial velocity
+            rb.velocity = new Vector3(initXVelocity, initYVelocity, initZVelocity);
+
+            //sets it so cannot launch multiple times in a frame until cooldown/event
+            hasLaunched = true;
+        }
+    }    
+}
---
+++ aero-simulator/Assets/flight.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Math = System.Math;
+
+public class flight : MonoBehaviour
+{
+    public Mesh wings;
+    public Rigidbody rb;
+    public Transform aircraft;
+    public Transform target;
+    float g = Physics.gravity.y;
+    float pi = Convert.ToSingle(Math.PI);
+    float Cl;
+    float densityOfAir = 1.225f;
+    float initVelocity= 185;
+    //defines the maximum thrust the engine can apply
+    float maxThrust=64000f;
+    float rotationSpeed = 20f;
+    float CalculateSurfaceArea(Mesh mesh) {
+        var triangles = mesh.triangles;
+        var vertices = mesh.vertices;
+        double sum = 0.0;
+        for(int i = 0; i < triangles.Length; i += 3) {
+            Vector3 corner = vertices[triangles[i]];
+            Vector3 a = vertices[triangles[i + 1]] - corner;
+            Vector3 b = vertices[triangles[i + 2]] - corner;
+            sum += Vector3.Cross(a, b).magnitude;
+        }
+        return (float)(sum/2.0);
+    }
+    void applyFlightForces(){
+        float weight = g * rb.mass;
+        //finds the angle of attack from the degree from the ground to the rotation of the plane in the local x direction
+        float angleOfAttack = -(aircraft.rotation.eulerAngles.x - 360f);
+        float currentForwardVelocity = aircraft.InverseTransformDirection(rb.velocity).z;
+        float currentYVelocity = rb.velocity.y;
+        angleOfAttack = angleOfAttack - .75f; 
+        //finds coefficient of lift (Cl)
+        if (angleOfAttack <= 12.5f || angleOfAttack>= -12.5){
+            Cl = ((angleOfAttack * pi)/180) * 2 * pi;
+        }
+        else if(12.6f <= angleOfAttack && angleOfAttack <15f || -12.6f >= angleOfAttack && angleOfAttack > -15f){
+            Cl = 15.1f/angleOfAttack;
+        }
+        else{
+            Cl = Convert.ToSingle(Math.Pow((15.1f/angleOfAttack),5));
+        }
+        //finds coefficient of drag using formula .01296*1.093^AoA*.75, with .75 being a bias
+        float Cd = .01296f*(Convert.ToSingle(Math.Pow(1.093,angleOfAttack)))*.75f;
+        //calculates the lift and drag applied on the airplan
+        float velocityZSquared = Convert.ToSingle(Math.Pow(currentForwardVelocity,2));
+        float lift = Cl * SA * .5f * densityOfAir * velocityZSquared;
+        float drag = Cd * SA * .5f * densityOfAir * velocityZSquared;
+        if(currentForwardVelocity<=55.555f && -angleOfAttack < -2.5f){
+            aircraft.Rotate((Vector3.right * rotationSpeed * Time.deltaTime));
+        }
+        float thrustApplied = maxThrust * .9f;
+        Debug.Log(lift);
+        Debug.Log(weight);
+        //makes flight ai recognize low speed and apply thrust until it reaches a set constant velocity (118.3m/s)
+        if(currentYVelocity < 0 && angleOfAttack > 0 || currentForwardVelocity<118.3){
+            rb.AddForce(transform.forward * thrustApplied);
+        }
+        rb.AddForce(transform.up * lift); 
+        if (currentForwardVelocity<0){  
+        rb.AddForce(transform.forward * (drag));
+        }
+        else{
+            rb.AddForce(transform.forward * (-drag));
+        }
+    }
+
+    public float SA;
+    void Start(){
+        SA = CalculateSurfaceArea(wings);
+        rb.velocity = transform.forward * initVelocity;
+    }
+    void FixedUpdate()
+    {   //sets aircraft speed and applies forces
+        applyFlightForces();
+    }
+}
